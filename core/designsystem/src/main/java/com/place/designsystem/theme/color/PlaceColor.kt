6ae604b6087b5a37a97a0a381da43d4918946dd7
khs3994 <dev.hyunsss@gmail.com>
2024-03-20 21:30:39 +0900

    :lipstick: :: add color theme

---
+++ core/designsystem/src/main/java/com/place/designsystem/theme/color/PlaceColor.kt
@@ -0,0 +1,66 @@
+package com.place.designsystem.theme.color
+
+import androidx.compose.ui.graphics.Color
+
+internal abstract class PlaceColor {
+
+    // black, white
+    abstract val black: Color
+    abstract val white: Color
+
+    // grey
+    abstract val grey1: Color
+    abstract val grey2: Color
+    abstract val grey3: Color
+    abstract val grey4: Color
+    abstract val grey5: Color
+    abstract val grey6: Color
+    abstract val grey7: Color
+    abstract val grey8: Color
+    abstract val grey9: Color
+    abstract val grey10: Color
+
+    // orange
+    abstract val orange1: Color
+    abstract val orange2: Color
+    abstract val orange3: Color
+    abstract val orange4: Color
+    abstract val orange5: Color
+    abstract val orange6: Color
+    abstract val orange7: Color
+    abstract val orange8: Color
+    abstract val orange9: Color
+
+    // green
+    abstract val green1: Color
+    abstract val green2: Color
+    abstract val green3: Color
+    abstract val green4: Color
+    abstract val green5: Color
+    abstract val green6: Color
+    abstract val green7: Color
+    abstract val green8: Color
+    abstract val green9: Color
+
+    // red
+    abstract val red1: Color
+    abstract val red2: Color
+    abstract val red3: Color
+    abstract val red4: Color
+    abstract val red5: Color
+    abstract val red6: Color
+    abstract val red7: Color
+    abstract val red8: Color
+    abstract val red9: Color
+
+    // blue
+    abstract val blue1: Color
+    abstract val blue2: Color
+    abstract val blue3: Color
+    abstract val blue4: Color
+    abstract val blue5: Color
+    abstract val blue6: Color
+    abstract val blue7: Color
+    abstract val blue8: Color
+    abstract val blue9: Color
+}
