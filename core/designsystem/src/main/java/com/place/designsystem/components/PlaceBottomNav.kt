dc548a4a155b74555059ee07069fa90183de1660
hyunseung0112 <hs.kim@fanmaum.com>
2024-04-01 20:55:45 +0900

    :lipstick: :: add gradation to bottom nav components

--- core/designsystem/src/main/java/com/place/designsystem/components/PlaceBottomNav.kt
+++ core/designsystem/src/main/java/com/place/designsystem/components/PlaceBottomNav.kt
@@ -2,17 +2,21 @@ package com.place.designsystem.components
 
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
@@ -42,6 +46,22 @@ fun PlaceBottomNav(
         Row(
             modifier = Modifier
                 .fillMaxWidth()
+                .clip(RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp))
+                .border(
+                    width = 2.dp,
+                    brush = Brush.verticalGradient(
+                        listOf(
+                            colors.grey2,
+                            colors.grey2,
+                            colors.grey1,
+                            colors.grey1,
+                            colors.grey1,
+                            colors.grey1,
+                            colors.grey1
+                        )
+                    ),
+                    shape = RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp)
+                )
                 .background(color = colors.grey1)
                 .padding(
                     start = 32.dp,
