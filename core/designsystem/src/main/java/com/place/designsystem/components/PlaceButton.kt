250a8c1de73c3c7c2000826199431326decc9116
hyunseung0112 <hs.kim@fanmaum.com>
2024-04-15 20:50:42 +0900

    :lipstick: :: modify button component

--- core/designsystem/src/main/java/com/place/designsystem/components/PlaceButton.kt
+++ core/designsystem/src/main/java/com/place/designsystem/components/PlaceButton.kt
@@ -1,64 +1,63 @@
 package com.place.designsystem.components
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.place.designsystem.effects.effectClickable
 import com.place.designsystem.theme.PlaceTheme
 
 @Composable
 fun PlaceFilledButton(
-    enabled: Boolean,
     modifier: Modifier = Modifier,
-    shape: Shape = RectangleShape,
-    paddingValues: PaddingValues = PaddingValues(5.dp),
+    enabled: Boolean,
     content: @Composable (RowScope.() -> Unit),
     onClick: () -> Unit,
 ) {
     PlaceTheme { colors, _ ->
-        Row(
-            modifier = modifier
-                .effectClickable(enabled = enabled, onClick = onClick)
-                .clip(shape = shape)
-                .background(color = if (enabled) colors.orange5 else colors.grey8)
-                .padding(paddingValues = paddingValues),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center,
-            content = content
+        Button(
+            modifier = modifier,
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colors.orange5,
+                disabledContainerColor = colors.grey8,
+                contentColor = colors.black,
+                disabledContentColor = colors.grey7
+            ),
+            enabled = enabled,
+            content = content,
+            onClick = onClick,
+            contentPadding = PaddingValues(vertical = 4.dp, horizontal = 8.dp)
         )
     }
 }
 
 @Composable
 fun PlaceOutlinedButton(
-    enabled: Boolean,
     modifier: Modifier = Modifier,
-    paddingValues: PaddingValues = PaddingValues(5.dp),
-    shape: Shape = RectangleShape,
+    enabled: Boolean,
     content: @Composable (RowScope.() -> Unit),
     onClick: () -> Unit,
 ) {
     PlaceTheme { colors, _ ->
-        Row(
-            modifier = modifier
-                .effectClickable(enabled = enabled, onClick = onClick)
-                .clip(shape = shape)
-                .border(width = 1.dp, color = colors.grey10, shape = shape)
-                .padding(paddingValues = paddingValues),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center,
-            content = content
+        OutlinedButton(
+            modifier = modifier,
+            shape = RoundedCornerShape(8.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Transparent,
+                disabledContainerColor = Color.Transparent,
+                contentColor = colors.white,
+                disabledContentColor = colors.grey7,
+            ),
+            enabled = enabled,
+            content = content,
+            onClick = onClick,
+            contentPadding = PaddingValues(vertical = 4.dp, horizontal = 8.dp)
         )
     }
 }
